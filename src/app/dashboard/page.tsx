97e001c1fbb73b42ef57234f7a89cedabdd49e7b
alviya-fatima <alviyafatima85@gmail.com>
2026-07-07 16:29:22 +0530

    gjkn

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,21 +1,43 @@
 "use client";
 
+import { useEffect, useState } from "react";
 import Image from "next/image";
 import dashboard from "../../assets/dashboard.png";
 
 import { auth } from "../firebase/config";
-import { signOut } from "firebase/auth";
+import {
+  signOut,
+  onAuthStateChanged,
+  User,
+} from "firebase/auth";
+
 import { useRouter } from "next/navigation";
 
 export default function Dashboard() {
   const router = useRouter();
+
+  const [user, setUser] = useState<User | null>(null);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      console.log(currentUser);
+
+      if (currentUser) {
+        setUser(currentUser);
+      } else {
+        router.push("/");
+      }
+    });
+
+    return () => unsubscribe();
+  }, [router]);
 
   const logout = async () => {
     try {
       await signOut(auth);
       router.push("/");
     } catch (error) {
-      console.error("Logout Error:", error);
+      console.error(error);
     }
   };
 
@@ -29,6 +51,19 @@ export default function Dashboard() {
         quality={100}
         className="dashboard-image"
       />
+
+      {user?.photoURL && (
+        <div className="profile-container">
+          <Image
+            src={user.photoURL}
+            alt="Profile"
+            width={140}
+            height={140}
+            unoptimized
+            className="profile-picture"
+          />
+        </div>
+      )}
 
       <button
         onClick={logout}
