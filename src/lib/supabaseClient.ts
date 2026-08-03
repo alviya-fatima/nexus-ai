e705d8784a4c9cf8732699c4ee4760538f7750fa
alviya-fatima <alviyafatima85@gmail.com>
2026-08-03 15:18:35 +0530

    ,

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://mqfvkeiyaymooszjgozc.supabase.co/rest/v1/";
+const supabaseUrl = "https://mqfvkeiyaymooszjgozc.supabase.co";
 const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1xZnZrZWl5YXltb29zempnb3pjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQyOTc1NzAsImV4cCI6MjA5OTg3MzU3MH0.tlJI5S3P0uvCnWU7K8SYwQbAOokwN4D_vpVdlSP3bbI";
 
 // Safe to use in both client components and server routes —
